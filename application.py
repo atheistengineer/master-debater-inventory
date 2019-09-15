bed4d1e9a5a56233e82a8d5c1740f38ce7ea2d29
Carl Fischer <Carl.Fischer.IV@gmail.com>
2019-09-15 10:04:33 -0400

    imported the ms sample

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
+import os, uuid, sys
+from azure.storage.blob import BlockBlobService, PublicAccess
 
 from flask import Flask
 app = Flask(__name__)
 
+# https://docs.microsoft.com/en-us/azure/app-service/containers/how-to-configure-python
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello Other World!"
